b62dd5525b80d2625879bce5b2bfdef38fc48cb3
Kat-Jester <hortonhandlesit@gmail.com>
2025-03-24 00:38:49 -0400

    Unfinished Broken Stage1

--- arrow.java
+++ arrow.java
@@ -8,12 +8,36 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class arrow extends Actor
 {
+    private int pulseTimer = 0;
+    private GreenfootImage baseArrow;
+    private double scale = 1.0;
+    private boolean growing = true;
+
     /**
      * Act - do whatever the arrow wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        baseArrow = new GreenfootImage("arrow.png");
+        pulse();
     }
-}
+    
+    private void pulse() {
+        // Adjust the scale amount and limits here
+        if (growing) {
+            scale += 0.02;
+            if (scale >= 1.2) growing = false;
+        } else {
+            scale -= 0.02;
+            if (scale <= 0.8) growing = true;
+        }
+
+        int newWidth = (int)(baseArrow.getWidth() * scale);
+        int newHeight = (int)(baseArrow.getHeight() * scale);
+
+        GreenfootImage scaled = new GreenfootImage(baseArrow);
+        scaled.scale(newWidth, newHeight);
+        setImage(scaled);
+    }
+}    
