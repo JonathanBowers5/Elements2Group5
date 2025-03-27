78d7e6524db5473455608628d5dfcc616d50e51a
kissidaniel842 <kissidaniel842@gmail.com>
2025-03-27 14:38:58 -0400

    added platforms
    added platforms

--- Stage2.java
+++ Stage2.java
@@ -15,13 +15,20 @@ public class Stage2 extends World
      */
     public Stage2()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+    // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+    super(600, 400, 1); 
         
-        GreenfootImage bg = new GreenfootImage("Stage 2 Back.png");
-        bg.scale(getWidth(), getHeight());
-        setBackground(bg);
-        
-        addObject(new brick.png (),300, 300);
+    GreenfootImage bg = new GreenfootImage("Stage 2 Back.png");
+    bg.scale(getWidth(), getHeight());
+    setBackground(bg);
+    
+    Platform platform1 = new Platform();
+    addObject(platform1, 100, 350);
+    
+    Platform platform2 = new Platform();
+    addObject(platform2, 300, 300);
+    
+    Platform platform3 = new Platform();
+    addObject(platform3, 500, 250);
     }
 }
